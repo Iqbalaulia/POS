e1d8fdd14eb2cddeb3fa816d1c98b342b7ad29a2
aulia <miqbalar16@gmail.com>
2018-08-15 11:28:23 +0700

    membuat kolom untuk tabel products2

--- database/migrations/2018_08_15_041226_create_table_products.php
+++ database/migrations/2018_08_15_041226_create_table_products.php
@@ -16,6 +16,10 @@ class CreateTableProducts extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('sku');
+$table->string('name');
+$table->integer('price');
+            
         });
     }
 
